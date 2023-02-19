84e250c6ab1980db2efae77bf0227b824a120ce7
drew <andrew.c.price@wsu.edu>
2023-02-18 23:28:30 -0800

    Impleneted playlist Maker with AI and UI

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from tkinter import *
 from tkinter import ttk
 from deepface import DeepFace as dp
 import cv2
-import matplotlib.pyplot as ppt
 import uuid
 import os
 
+
 root = Tk()
-root.geometry('500x500')
 root.title("Musical Recognition")
 
 
-def picture():
+def pic():
     REAL_PATH = os.path.join('data', 'tests')
     capture = cv2.VideoCapture(0)
     while capture.isOpened():
@@ -40,19 +39,23 @@ def picture():
     main_emotion = demo[0]['dominant_emotion']  
     if main_emotion == 'discust':
         print("No time for music! Your main emotion is " + main_emotion +  " Go exercise.")
-    else:
-        print("Your main emotion was " + main_emotion)
+    # else:
+    #     print("Your main emotion was " + main_emotion)
 
 def login():
-    b1 = ttk.Button(frm, text="Take Picture", command= picture).grid(column=2, row=1)
+    import playlistMaker as pm
+    # global ACCESS_TOKEN
+    # ACCESS_TOKEN = pm.user_authorize_Spotipy()
+    print(pm.ACCESS_TOKEN)
+    b1 = ttk.Button(frm, text="Take Picture", command= pic).grid(column=2, row=1)
     ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=7)
 
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 
-ttk.Label(frm, text="Musical Recognition").grid(column=0, row=0)
-
 b0=ttk.Button(frm, text = "Login", command = login).grid(column=2, row=1)
 
 
 root.mainloop()
+
+
--- playlistMaker.py
+++ playlistMaker.py
@@ -12,6 +12,7 @@ USER_PLAYLISTS_URL = 'https://api.spotify.com/v1/me/playlists'
 AUTHORIZATION_URL = 'https://accounts.spotify.com/authorize?'
 CODE = 'AQC_56RK0ZUzL1kNNMyp8CW6qM3F7ELH2AQ51cu5CW2gkD4p8lsFutWgAhVfjBdc_rvqfdpi2EOLgNfH4ALRz0J5Jq'
 
+
 auth_headers = {
     "client_id": CLIENT_ID,
     "response_type": "code",
@@ -21,26 +22,26 @@ auth_headers = {
 
 def user_authorize_Spotipy():
     ACCESS_TOKEN = spotipy.util.prompt_for_user_token(username='',scope='playlist-modify-public playlist-modify-private user-library-read',client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri=REDIRECT_URI)
-    print("Access Token: {}".format(ACCESS_TOKEN))
+    # print("Access Token: {}".format(ACCESS_TOKEN))
     return ACCESS_TOKEN
 
-def user_authorize():
-    webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
-    encoded_credentials = base64.b64encode(CLIENT_ID.encode() + b':' + CLIENT_SECRET.encode()).decode("utf-8")
-    token_headers = {
-        "Authorization": "Basic " + encoded_credentials,
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    token_data = {
-        "grant_type": "authorization_code",
-        "code": CODE,
-        "redirect_uri": REDIRECT_URI
-    }  
-    response = requests.post("https://accounts.spotify.com/api/token", data=token_data, headers=token_headers)
-    json_resp = response.json()
-    ACCESS_TOKEN = json_resp["access_token"]
-    # print(json_resp)
-    return ACCESS_TOKEN
+# def user_authorize():
+#     webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
+#     encoded_credentials = base64.b64encode(CLIENT_ID.encode() + b':' + CLIENT_SECRET.encode()).decode("utf-8")
+#     token_headers = {
+#         "Authorization": "Basic " + encoded_credentials,
+#         "Content-Type": "application/x-www-form-urlencoded"
+#     }
+#     token_data = {
+#         "grant_type": "authorization_code",
+#         "code": CODE,
+#         "redirect_uri": REDIRECT_URI
+#     }  
+    # response = requests.post("https://accounts.spotify.com/api/token", data=token_data, headers=token_headers)
+    # json_resp = response.json()
+    # ACCESS_TOKEN = json_resp["access_token"]
+    # # print(json_resp)
+    # return ACCESS_TOKEN
 
 def create_playlist(name, public):
     response = requests.post(USER_PLAYLISTS_URL, 
@@ -70,15 +71,12 @@ def read_saved_tracks():
     #print(json_resp['items'])
     return json_resp
 
-def main():
-    global ACCESS_TOKEN
-    ACCESS_TOKEN = user_authorize_Spotipy()
-    #newPlaylist = create_playlist(name = "Silly Ahh", public=False)
-    # print("Playlist: {}".format(newPlaylist))
-    #newPlaylistURL = newPlaylist['tracks']['href']
-    sillyGoofyMood = ['spotify:track:3xZek9XkEaX130o3XN9cvd', 'spotify:track:28UMEtwyUUy5u0UWOVHwiI', 'spotify:track:02JIdsrod3BYucThfUFDUX', 'spotify:track:4Cd01GWLuMTNZhW0DE7cF4', 'spotify:track:1rKBOL9kJfX1Y4C3QaOvRH', 'spotify:track:3hSs17SMNlysCjVWkaJPFd']
-    #add_to_playlist(sillyGoofyMood, newPlaylistURL)
-    read_saved_tracks()
+global ACCESS_TOKEN
+ACCESS_TOKEN = user_authorize_Spotipy()
+#     #newPlaylist = create_playlist(name = "Silly Ahh", public=False)
+#     # print("Playlist: {}".format(newPlaylist))
+#     #newPlaylistURL = newPlaylist['tracks']['href']
+#     sillyGoofyMood = ['spotify:track:3xZek9XkEaX130o3XN9cvd', 'spotify:track:28UMEtwyUUy5u0UWOVHwiI', 'spotify:track:02JIdsrod3BYucThfUFDUX', 'spotify:track:4Cd01GWLuMTNZhW0DE7cF4', 'spotify:track:1rKBOL9kJfX1Y4C3QaOvRH', 'spotify:track:3hSs17SMNlysCjVWkaJPFd']
+#     #add_to_playlist(sillyGoofyMood, newPlaylistURL)
+#     read_saved_tracks()
 
-if __name__ == '__main__':
-    main()
