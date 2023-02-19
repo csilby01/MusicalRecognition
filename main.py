10902c175fdaad4c56bc91bdfd682e03cb514caf
jsantos1 <jerel-tsar.santos@wsu.edu>
2023-02-19 02:28:19 -0800

    Done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+import webbrowser
 from deepface import DeepFace as dp
 import cv2
 import uuid
@@ -9,12 +9,14 @@ import playlistMaker as pm
 
 
 
+
 root = Tk()
 root.configure(bg = "green")
 root.geometry('300x300')
 root.title("Musical Recognition")
 
-
+def callback(url):
+    webbrowser.open_new(url)
 def pic():
     REAL_PATH = os.path.join('data', 'tests')
     capture = cv2.VideoCapture(0)
@@ -44,11 +46,21 @@ def pic():
     main_emotion = demo[0]['dominant_emotion']  
     if main_emotion == 'disgust':
         print("No time for music! Your main emotion is " + main_emotion +  " Go exercise.")
-    elif main_emotion == 'happy' or main_emotion == 'sad':
-        print("Your main emotion was " + main_emotion)
-        pm.create_mood_playlist(main_emotion)   
+        l0=Label(root, text = "No time for music! your main emotion is disgust. Go exercise.")
+        l0.pack()
+    elif main_emotion == 'happy' or main_emotion == 'sad' or main_emotion == 'fear' or main_emotion == 'angry':
+        hyperlink = pm.create_mood_playlist(main_emotion)
+        l1=Label(root, text = "Click here to view your Playlist", fg="blue", cursor="hand2", underline=31)#.grid(column=2,row=1)
+        l1.pack()
+        l1.bind("<Button-1>", lambda x: callback(hyperlink))
+        print("Your emotion was " + main_emotion)
+        
     else:
-        print ("get a life")
+        l2=Label(root, text = "retry")
+        l2.pack()
+    str = "Your emotion is {}".format(main_emotion)
+    l3=Label(root, text = str)
+    l3.pack(side = BOTTOM)
 
 def login():
     settings.ACCESS_TOKEN = pm.user_authorize_Spotipy()
@@ -56,14 +68,13 @@ def login():
     # global ACCESS_TOKEN
     b1 = Button(frm, text="Take Picture", command= pic, highlightbackground="green", fg = "blue").grid(column=2, row=1)
     b2 = Button(frm, text="Quit", command=root.destroy, highlightbackground="green", fg = "red").grid(column=2, row=7)
+    #b2.pack(side=BOTTOM)
 
 frm = Frame(root,bg="green")
 frm.grid()
 frm.pack()
 
 b0= Button(frm, text = "Login", command = login, highlightbackground="green", fg = "green").grid(column=2, row=1)
-
-
 root.mainloop()
 
 
